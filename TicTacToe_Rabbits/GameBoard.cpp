ce5afaac20715cfd9cf637d53f11d9a77d0f33bc
Dillon Pontes <55772248+pontesda@users.noreply.github.com>
2021-09-10 22:31:38 -0400

    Test

--- TicTacToe_Rabbits/GameBoard.cpp
+++ TicTacToe_Rabbits/GameBoard.cpp
@@ -3,44 +3,6 @@
 
 //defining functions created in the .h file
 
-void GameBoard::GameBoardDisplay()
-{
-	std::cout << "   ";
-	for (int x = 0; x < 3; x++)
-	{
-		std::cout << grid[x];
-		if (x < 2)
-		{
-			cout << "|  ";
-		}
-
-	}
-	std::cout << std::endl;
-	std::cout << "  ";
-	std::cout << "----------"<<std::endl;
-	std::cout << "   ";
-	for (int x = 3; x < 6; x++)
-	{
-		std::cout << grid[x];
-		if (x < 5)
-		{
-			cout << "|  ";
-		}
-	}
-	std::cout << std::endl;
-	std::cout << "  ";
-	std::cout << "----------" << std::endl;
-	std::cout << "   ";
-	for (int x = 6; x < 9; x++)
-	{
-		std::cout << grid[x];
-		if (x < 8)
-		{
-			cout << "|  ";
-		}
-	}
-	
-	std::cout << std::endl;
-
-	
-}
+void GameBoard::GameBoardDisplay() {};
+void GameBoard::GameBoardUpdate() {};
+void GameBoard::GameBoardReset() {};
--- TicTacToe_Rabbits/GameBoard.h
+++ TicTacToe_Rabbits/GameBoard.h
@@ -7,24 +7,9 @@ class GameBoard
 {
 private:
 public:
-	//constructors
-	//mutators
-	//accessors
-	GameBoard()//default constructor
-	{
-
-		for (int x = 0; x <= 8; x++)
-		{
-			grid[x] = ' ';
-		}
-	}
-
-	void GameBoardDisplay();//accessor function to display the current game board
-
-	
-							// accessor function to check if the current board has a winner. (if winner, state and end game)
-	//function to track the status of the board. possibly in an array.
+	void GameBoardDisplay();
+	void GameBoardUpdate();
+	void GameBoardReset();
 	//function that prompts players to make a move
-
 };
 #endif
